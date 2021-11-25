37066f6145bae7ad2f084f77d629c66a935849a0
IsabelMaPi <a_isabel.maye.piulestan@iespablopicasso.es>
2021-11-25 14:26:40 +0100

    programa bien

--- egit/src/egit/arraynumrandom.java
+++ egit/src/egit/arraynumrandom.java
@@ -3,15 +3,15 @@ package egit;
 public class arraynumrandom {
 
 	public static void main(String[] args) {
-		int array[] = new int[20];
-//		int arraycont[] = new int[10];
+		int array[] = new int[30];
+		int arraycont[] = new int[10];
 		for (int i = 0; i < array.length; i++) {
 			array[i] = (int) (Math.random() * 10 + 1);
 			System.out.print("\t" + array[i]);
-//			arraycont[array[i]-1]=arraycont[0]+1;
+			arraycont[array[i]-1]=arraycont[0]+1;
 		}
-//		for (int i = 0; i < arraycont.length; i++) {
-//			System.out.print("\n"+(i+1)+": "+arraycont[i]);
-//		}
+		for (int i = 0; i < arraycont.length; i++) {
+			System.out.print("\n"+(i+1)+": "+arraycont[i]);
+		}
 	}
 }
